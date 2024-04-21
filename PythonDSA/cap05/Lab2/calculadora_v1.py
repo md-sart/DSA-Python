084e2bfea5500761d9ed4ec341d01db42c75252a
Maria <110576299+md-sart@users.noreply.github.com>
2024-04-21 19:42:55 +0000

    .

--- PythonDSA/cap05/Lab2/calculadora_v1.py
+++ PythonDSA/cap05/Lab2/calculadora_v1.py
@@ -5,16 +5,49 @@
 
 print("\n******************* Bem vindo a Calculadora em Python *******************")
 
-num1 = float(input("Entre com o primeiro valor"))
-num2 = float(input("Entre com o segundo valor"))
-operacao = str(input("Agora, escolha entre as operações: +, -, / ou *"))
+while True:
+    try:
+        num1 = float(input("Entre com o primeiro valor: "))
+        break 
+    except ValueError:
+        print("Por favor, insira um valor numérico.")
+
+while True:
+    try:
+        num2 = float(input("Entre com o segundo valor: "))
+        break 
+    except ValueError:
+        print("Por favor, insira um valor numérico.")
+
+while True:
+    operacao = str(input("Agora, escolha entre as operações: +, -, / ou *: "))
+    if operacao in ['+', '-', '/', '*']:
+        break
+    else:
+        print("Por favor, digite um operador válido.")
 
 def soma(x,y):
- resultado = x+y
- return(resultado)
+  resultado = x+y
+  return(resultado)
+
+def subtracao(x,y):
+  resultado = x-y
+  return(resultado)
+
+def multi(x,y):
+  resultado = x*y
+  return(resultado)
+
+def divisao(x,y):
+  resultado = x/y
+  return(resultado)
 
 if operacao == "+":
     resultado = soma(num1, num2)
+    print ("O resultado desta operação é: ", resultado)
+
+elif operacao == "-":
+    resultado = subtracao(num1, num2)
     print ("O resultado desta operação é: ", resultado)
 
  #autor: neste laboratório quis fazer a calculadora de uma maneira diferente. utilizei funções para chamar as operações 
